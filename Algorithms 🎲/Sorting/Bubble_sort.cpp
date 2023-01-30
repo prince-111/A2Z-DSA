2b20b952937c37bfc6b8a3561c159bf745b75ef8
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-30 07:51:26 +0000

    number sort using Bubble_sort 🎈

---
+++ Algorithms 🎲/Sorting/Bubble_sort.cpp
@@ -0,0 +1,45 @@
+// Bubble sort.
+// Time Complexity:- 0(N^2) worst & average and 0(N) best
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = n - 1; i >= 0; i--)
+    {
+        int didSwap = 0;
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if (didSwap == 0)
+        {
+            break;
+        }
+        cout << "run\n";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    bubble_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
