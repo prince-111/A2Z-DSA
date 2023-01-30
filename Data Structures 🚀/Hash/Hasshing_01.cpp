592b2cb38eae745576f9eb7c1a6daf39e05b3558
Prince Kumar <88477312+prince-111@users.noreply.github.com>
2023-01-30 07:41:27 +0000

    Basic hashing counting character🐤

---
+++ Data Structures 🚀/Hash/Hasshing_01.cpp
@@ -0,0 +1,36 @@
+// basic hashing.
+// Time Complexity:- 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s;
+	cin>>s;
+	
+	// pre compute
+	int hash[256]={0};
+	for(int i=0; i<s.size(); i++){
+	    hash[s[i]]++;
+	}
+	
+	int q;
+	cin>>q;
+	
+	while(q--){
+	    char c;
+	    cin>>c;
+	    // fetch
+	    cout<<hash[c]<<endl;
+	}
+	return 0;
+}
+/*
+abcdabehf
+5
+a
+g
+h
+b
+c
+*/
